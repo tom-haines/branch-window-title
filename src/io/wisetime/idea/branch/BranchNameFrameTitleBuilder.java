5c4f6feae87a5fa161ac7817226e82e65c796647
Tom Haines <thomas.haines@practiceinsight.io>
2017-05-01 00:18:59 +0800

    Code cleanup; add comments; add links; rename vars to match functions

--- src/io/wisetime/idea/branch/BranchNameFrameTitleBuilder.java
+++ src/io/wisetime/idea/branch/BranchNameFrameTitleBuilder.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileAdapter;
 import com.intellij.openapi.vfs.VirtualFileEvent;
+import com.intellij.openapi.vfs.VirtualFileListener;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.openapi.wm.impl.FrameTitleBuilder;
@@ -21,30 +22,60 @@ import java.util.regex.Pattern;
 public class BranchNameFrameTitleBuilder extends FrameTitleBuilder {
   private static FrameTitleBuilder defaultBuilder;
 
-  private static final String REF_PREFIX = "refs/";
-  private static final String HEAD_PREFIX = "heads/";
+  // static singleton of thread-safe Pattern object
   private static final Pattern FIRST_LINE_PATTERN = Pattern.compile("(ref: )?(.+)[\\r\\n]*");
 
-  public static void setDefaultBuilder(FrameTitleBuilder defaultBuilder) {
+  // maintains list of unique .git/HEAD file paths being watched
+  private final Set<String> gitHeadFileWatchSet;
+
+  public BranchNameFrameTitleBuilder() {
+    gitHeadFileWatchSet = new HashSet<String>();
+  }
+
+  static void setDefaultBuilder(FrameTitleBuilder defaultBuilder) {
     BranchNameFrameTitleBuilder.defaultBuilder = defaultBuilder;
   }
 
-  private Set<String> headFilesListeningTo = new HashSet<String>();
-
-  // watch the root pom.xml for changes, if it exists
+  /**
+   * watch the root .git/HEAD for changes (if it exists)
+   *
+   * @return gitHeadFile if exists or null otherwise.
+   */
   private VirtualFile watchThisProject(Project project) {
     VirtualFile gitDir = project.getBaseDir().findChild(".git");
     if (gitDir != null) {
-      VirtualFile headFile = gitDir.findChild("HEAD");
-      if (headFile != null) {
-        registerFileChangedListener(headFile.getCanonicalPath());
-        return headFile;
+      VirtualFile gitHeadSymRefFile = gitDir.findChild("HEAD");
+      if (gitHeadSymRefFile != null) {
+        registerFileChangedListener(gitHeadSymRefFile.getCanonicalPath());
+        return gitHeadSymRefFile;
       }
     }
     return null;
   }
 
-  // this fires once when a project is opened, use this hook to watch the project's pom for changes
+  /**
+   * Maintain watch on gitHeadFile via {@link VirtualFileManager#addVirtualFileListener(VirtualFileListener)}.
+   */
+  private void registerFileChangedListener(final String gitHeadFilePath) {
+    if (!gitHeadFileWatchSet.contains(gitHeadFilePath)) {
+      gitHeadFileWatchSet.add(gitHeadFilePath);
+      VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileAdapter() {
+        @Override
+        public void contentsChanged(@NotNull VirtualFileEvent event) {
+          if (gitHeadFilePath.equals(event.getFile().getCanonicalPath())) {
+            final String branchName = determineBranchName(event.getFile());
+            updateFrameTitle(getProjectForFile(event.getFile()), branchName);
+          }
+        }
+      });
+    }
+  }
+
+  /**
+   * This fires once when a project is opened.  The hook is used to setup a watch for .git/HEAD changes.
+   *
+   * @return The project title to use for the given project.
+   */
   @Override
   public String getProjectTitle(@NotNull Project project) {
     String branchName = null;
@@ -57,7 +88,10 @@ public class BranchNameFrameTitleBuilder extends FrameTitleBuilder {
     return createProjectTitle(project, branchName);
   }
 
-  private String createProjectTitle(Project project, String branchName) {
+  /**
+   * Given a branchName and a project, create a modified project name.
+   */
+  private String createProjectTitle(@NotNull Project project, String branchName) {
     if (branchName != null) {
       // decorate the default builder
       return defaultBuilder.getProjectTitle(project).replace(" [", " " + branchName + " - [");
@@ -66,52 +100,32 @@ public class BranchNameFrameTitleBuilder extends FrameTitleBuilder {
     }
   }
 
-  // no change
+  /**
+   * Use default builder implementation (no change).
+   */
   @Override
   public String getFileTitle(@NotNull Project project, @NotNull VirtualFile virtualFile) {
     return defaultBuilder.getFileTitle(project, virtualFile);
   }
 
-  private void registerFileChangedListener(final String headFileCanonicalPath) {
-    if (!headFilesListeningTo.contains(headFileCanonicalPath)) {
-      headFilesListeningTo.add(headFileCanonicalPath);
-      VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileAdapter() {
-        @Override
-        public void contentsChanged(@NotNull VirtualFileEvent event) {
-          if (headFileCanonicalPath.equals(event.getFile().getCanonicalPath())) {
-            final String branchName = determineBranchName(event.getFile());
-            updateFrameTitle(getProjectForFile(event.getFile()), branchName);
-          }
-        }
-      });
-    }
-  }
-
   private String determineBranchName(VirtualFile headFile) {
-    if (headFile == null) {
-      return null;
-    }
-
-    try {
-      String headLinkAsString = new String(headFile.contentsToByteArray());
-      Matcher matcher = FIRST_LINE_PATTERN.matcher(headLinkAsString);
-      if (matcher.find() && matcher.groupCount() >= 2) {
-        String branchName = matcher.group(2).trim();
-        branchName = removePrefix(REF_PREFIX, branchName);
-        branchName = removePrefix(HEAD_PREFIX, branchName);
-        return branchName;
+    if (headFile != null) {
+      try {
+        String headLinkAsString = new String(headFile.contentsToByteArray());
+        Matcher matcher = FIRST_LINE_PATTERN.matcher(headLinkAsString);
+        if (matcher.find() && matcher.groupCount() >= 2) {
+          String branchName = matcher.group(2).trim();
+          branchName = removePrefix("refs/", branchName);
+          branchName = removePrefix("heads/", branchName);
+          return branchName;
+        }
+      } catch (Exception e) {
+        // ignore
       }
-    } catch (Exception e) {
-      // ignore...
     }
-    return null;
-  }
 
-  private String removePrefix(String prefix, String branchName) {
-    if (branchName.startsWith(prefix) && branchName.length() > prefix.length()) {
-      return branchName.substring(prefix.length());
-    }
-    return branchName;
+    // could not determine branch name, returning null
+    return null;
   }
 
   private void updateFrameTitle(Project project, String branchName) {
@@ -121,21 +135,35 @@ public class BranchNameFrameTitleBuilder extends FrameTitleBuilder {
     }
   }
 
-  private Project getProjectForFile(VirtualFile pomFile) {
-    // look through all open projects and see if this file is contained in it
+  /**
+   * Look through all open projects and see if git head symlink file is contained in it.
+   */
+  private Project getProjectForFile(VirtualFile gitHeadFile) {
+    //
     for (Project project : ProjectManager.getInstance().getOpenProjects()) {
       try {
         VirtualFile[] contentRootArray = ProjectRootManager.getInstance(project).getContentRoots();
         for (VirtualFile virtualFile : contentRootArray) {
           String expectedLoc = virtualFile.getCanonicalPath() + "/.git/HEAD";
-          if (expectedLoc.equals(pomFile.getCanonicalPath())) {
+          if (expectedLoc.equals(gitHeadFile.getCanonicalPath())) {
             return project;
           }
         }
       } catch (Exception e) {
-        // ignore... directory index might not be initialized yet
+        // ignore
       }
     }
     return null;
   }
+
+  /**
+   * Utility function to tidy-up common repetition of sym-link path to current branch.
+   */
+  private String removePrefix(String prefix, String branchName) {
+    if (branchName.startsWith(prefix) && branchName.length() > prefix.length()) {
+      return branchName.substring(prefix.length());
+    }
+    return branchName;
+  }
+
 }
