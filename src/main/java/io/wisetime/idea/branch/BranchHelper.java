771950013f0174a5b2d9e68b7f9688224236de35
vadym <vsukolen@anaqua.com>
2023-10-07 14:40:28 +0800

    branch title info provider

--- src/main/java/io/wisetime/idea/branch/BranchHelper.java
+++ src/main/java/io/wisetime/idea/branch/BranchHelper.java
@@ -1,23 +1,12 @@
 package io.wisetime.idea.branch;
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ex.ProjectEx;
-import com.intellij.openapi.wm.WindowManager;
-import com.intellij.openapi.wm.impl.FrameTitleBuilder;
-import javax.swing.JFrame;
+import com.intellij.openapi.components.Service;
+import com.intellij.openapi.components.Service.Level;
 
+@Service(Level.PROJECT)
 public final class BranchHelper {
 
-  private final Logger logger = Logger.getInstance("FrameHelper");
-
-  private final Project project;
   private String currentBranchName;
-
-  public BranchHelper(Project project) {
-    this.project = project;
-  }
 
   public String getCurrentBranchName() {
     return currentBranchName;
@@ -25,34 +14,6 @@ public final class BranchHelper {
 
   public void onBranchChanged(String branchName) {
     currentBranchName = branchName;
-    ApplicationManager.getApplication().invokeLater(this::updateFrameTitle);
-  }
-
-  private void updateFrameTitle() {
-    try {
-      final String projectTitle = FrameTitleBuilder.getInstance().getProjectTitle(project);
-      updateProjectTitle(projectTitle);
-      updateWindowTitle(projectTitle);
-    } catch (Exception e) {
-      logger.error("error while updating title", e);
-    }
-  }
-
-  private void updateWindowTitle(String projectTitle) {
-    JFrame ideFrame = WindowManager.getInstance().getFrame(project);
-    if (ideFrame != null) {
-      ideFrame.setTitle(projectTitle);
-    } else {
-      logger.info("unable to obtain IdeFrame (WindowManager returned null ideFrame)");
-    }
-  }
-
-  private void updateProjectTitle(String projectTitle) {
-    if (project instanceof ProjectEx) {
-      ((ProjectEx) project).setProjectName(projectTitle);
-    } else {
-      logger.warn("Can't update branch title to " + project);
-    }
   }
 
 }
